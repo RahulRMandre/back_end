e05f781e0c90d9ae3eb0a72f99635b423ed916e8
SUBRAMANYA U S <ussubramanya@gmail.com>
2019-03-06 23:55:48 +0530

    Build successful 06022019

--- src/main/java/com/ss/food/FoodApplication.java
+++ src/main/java/com/ss/food/FoodApplication.java
@@ -1,17 +1,122 @@
 package com.ss.food;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.actuate.autoconfigure.MetricFilterAutoConfiguration;
+//import org.springframework.boot.actuate.autoconfigure.MetricRepositoryAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
+
+import com.google.common.base.Joiner;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
 import com.ss.food.config.AppProperties;
+import com.ss.food.config.Constants;
 
 @SpringBootApplication(scanBasePackages={"com.ss"})
 @EnableConfigurationProperties(AppProperties.class)
+@ComponentScan(basePackages  = {"com.ss"})
+@EnableJpaRepositories(basePackages  = {"com.ss"})
+@EntityScan(basePackages  = {"com.ss"})
+//@EnableAutoConfiguration(exclude = {MetricFilterAutoConfiguration.class, MetricRepositoryAutoConfiguration.class})
 public class FoodApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(FoodApplication.class);
+	
+	@Inject
+    private Environment env;
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoodApplication.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(FoodApplication.class, args);
+//	}
+	
+    @PostConstruct
+    public void initApplication() throws IOException {
+        if (env.getActiveProfiles().length == 0) {
+            log.warn("No Spring profile configured, running with default configuration");
+        } else {
+            log.info("Running with Spring profile(s) : {}", Arrays.toString(env.getActiveProfiles()));
+            Collection activeProfiles = Arrays.asList(env.getActiveProfiles());
+            if (activeProfiles.contains("dev") && activeProfiles.contains("prod")) {
+                log.error("You have misconfigured your application! " +
+                    "It should not run with both the 'dev' and 'prod' profiles at the same time.");
+            }
+            if (activeProfiles.contains("prod") && activeProfiles.contains("fast")) {
+                log.error("You have misconfigured your application! " +
+                    "It should not run with both the 'prod' and 'fast' profiles at the same time.");
+            }
+            if (activeProfiles.contains("dev") && activeProfiles.contains("cloud")) {
+                log.error("You have misconfigured your application! " +
+                    "It should not run with both the 'dev' and 'cloud' profiles at the same time.");
+            }
+        }
+    }
+//
+//    /**
+//     * Main method, used to run the application.
+//     */
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(FoodApplication.class);
+        //app.setShowBanner(false);
+        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+        addDefaultProfile(app, source);
+        addLiquibaseScanPackages();
+        Environment env = app.run(args).getEnvironment();
+        log.info("Access URLs:\n----------------------------------------------------------\n\t" +
+            "Local: \t\thttp://127.0.0.1:{}\n\t" +
+            "External: \thttp://{}:{}\n----------------------------------------------------------",
+            env.getProperty("server.port"),
+            InetAddress.getLocalHost().getHostAddress(),
+            env.getProperty("server.port"));
 
+    }
+//
+//    /**
+//     * If no profile has been configured, set by default the "dev" profile.
+//     */
+    private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
+        if (!source.containsProperty("spring.profiles.active") &&
+                !System.getenv().containsKey("SPRING_PROFILES_ACTIVE")) {
+
+            app.setAdditionalProfiles(Constants.SPRING_PROFILE_DEVELOPMENT);
+        }
+    }
+//
+//    /**
+//     * Set the liquibases.scan.packages to avoid an exception from ServiceLocator.
+//     */
+    private static void addLiquibaseScanPackages() {
+        System.setProperty("liquibase.scan.packages", Joiner.on(",").join(
+            "liquibase.change", "liquibase.database", "liquibase.parser",
+            "liquibase.precondition", "liquibase.datatype",
+            "liquibase.serializer", "liquibase.sqlgenerator", "liquibase.executor",
+            "liquibase.snapshot", "liquibase.logging", "liquibase.diff",
+            "liquibase.structure", "liquibase.structurecompare", "liquibase.lockservice",
+            "liquibase.ext", "liquibase.changelog"));
+    }
+    
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    
+	
+	
 }
